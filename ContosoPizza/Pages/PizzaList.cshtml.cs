0d3aa84864ca76a6f37f92623a4bb980b4d9630b
Pritom Ahmad <135753963+pritom1424@users.noreply.github.com>
2024-10-27 18:32:18 +0600

    full crud frontend

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -16,5 +16,22 @@ namespace ContosoPizza.Pages
         public void OnGet(){
             PizzaList = _service.GetPizzas();
         }
+
+        [BindProperty]
+        public Pizza NewPizza{get;set;}=default!;
+
+        public IActionResult OnPost(){
+            if(!ModelState.IsValid|| NewPizza==null){
+                return Page();
+            }
+            _service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
     }
 }
